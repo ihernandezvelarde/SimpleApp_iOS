176ae9fa6223524ae08d54d1a96dc6db96f6f33e
ihernandezvelarde <neeiihernandez@gmail.com>
2022-06-22 19:22:09 +0200

    Initial commit

---
+++ SimpleApp_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleApp_iOS
+//
+//  Created by Irene Hernández on 22/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
